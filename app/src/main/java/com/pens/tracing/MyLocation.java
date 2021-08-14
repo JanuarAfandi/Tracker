13785e53f6a9ec49321468397e9a9684f1223b40
JanuarAfandi <januarafandi01@gmail.com>
2021-08-14 13:12:10 +0700

    First Commit
    Tracing app

---
+++ app/src/main/java/com/pens/tracing/MyLocation.java
@@ -0,0 +1,28 @@
+package com.pens.tracing;
+
+public class MyLocation {
+    private double latitude;
+    private double logitude;
+
+    public MyLocation(double latitude, double logitude){
+        this.latitude = latitude;
+        this.logitude = logitude;
+    }
+    public MyLocation()
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLogitude() {
+        return logitude;
+    }
+
+    public void setLogitude(double logitude) {
+        this.logitude = logitude;
+    }
+}
